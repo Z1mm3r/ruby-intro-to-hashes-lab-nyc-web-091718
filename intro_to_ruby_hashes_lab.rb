317a0b5040da939e486ed8a23ca72812d507d17a
Casey Zimmer <czimmer@ramapo.edu>
2018-09-16 19:35:55 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,9 @@ hash
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	hash = monopoly_with_third_tier
+	hash[:railroads][:names][:reading_railroad][:mortgage_value] = '$100'
+	
 	
 end
 
